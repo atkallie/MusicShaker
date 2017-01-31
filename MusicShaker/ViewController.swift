a34faa43b565384d21c8d56072e437b57465aea5
atkallie <kal17da@gmail.com>
2017-01-31 13:33:53 -0500

    Initial Commit

--- MusicShaker/ViewController.swift
+++ MusicShaker/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player = AVAudioPlayer()
+    let songs = ["LetMeLoveYou","ISeeFire","ILikeBadHouseVersion","INeedLove"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake{
+            let randomSong = Int(arc4random_uniform(4))
+            
+            let audioPath = Bundle.main.path(forResource: songs[randomSong], ofType: "mp3")
+            
+            do{
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+                player.play()
+            }catch{
+                //process errors
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
